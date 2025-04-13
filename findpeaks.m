7805f0b332659759a618143160b8f1225f150a45
Leviathan <levi.mcgomes@gmail.com>
2025-04-13 17:57:19 +0100

    feat: add function findpeaks to locate the peaks in a signal

---
+++ findpeaks.m
@@ -0,0 +1,45 @@
+## -*- texinfo -*-
+## @deftypefn {Function File} {[@var{pks} @var{locs}] =} findpeaks (@var{data}, @var{height}, @var{dist})
+## Find peaks (local maxima) in data.
+##
+## @var{pks} is the peak values, @var{locs} is the indices
+## of the peaks.
+##
+## @var{data} is the input data, @var{height} is the minimum
+## peak height, and @var{dist} is the minimum distance
+## between consecutive peaks.
+##
+## @var{pks} is a vector of the same kind as @var{data} and
+## @var{locs} is a row vector.
+##
+## Taken from https://stackoverflow.com/a/77410765/22222542
+## @end deftypefn
+function [pks locs] = findpeaks(data, height, dist)
+    narginchk(3, 3);
+    nargoutchk(1, 2);
+
+    # find all peaks
+    locs = regexp(char(sign(diff(reshape(data, 1, []))) + '1'), '21*0') + 1;
+    # apply MinPeakHeight
+    locs = locs(data(locs) > height);
+    # apply MinPeakDistance
+    [~, isorted] = sort(data(locs), 'descend');
+    n = numel(data);
+    idx = false(1, n);
+    idx(locs) = true;
+
+    for s = reshape(locs(isorted), 1, [])
+
+        if (idx(s))
+            idx(max(s - dist, 1):min(s + dist, n)) = 0;
+            idx(s) = 1;
+        end
+
+    end
+
+    locs = find(idx);
+    pks = data(locs);
+
+    size(pks)
+    size(locs)
+end
